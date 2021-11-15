c7dd8a538e1f0250ccad7b4dc7632f579c58004c
Grupo-N-6 <90696766+Grupo-N-6@users.noreply.github.com>
2021-11-14 21:27:32 -0300

    Add files via upload

---
+++ js/funcionesGrupo99.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    alert("HAS INGRESADO A LA BIBLIOTECA");
+}
+
+function cambiarImagen() {
+    const Imagen = document.getElementById("foto")
+    Imagen.src = "images/young.jpg"
+
+}
+
+function mostrarOriginal() {
+    const imagen = document.getElementById("foto")
+    imagen.src = "images/libros.jpg"
+
+}
